6359c07e845d8ffb99f5af5a435b495838c423a3
なまりゆか <namariko29@gmail.com>
2019-10-16 02:37:36 +0900

    test

---
+++ WEB-INF/src/.bash_profile
@@ -0,0 +1,1 @@
+export CLASSPATH=.:/usr/local/Cellar/tomcat/9.0.27/libexec/lib/servlet-api.jar
---
+++ WEB-INF/src/chapter3/Hello2.java
@@ -0,0 +1,31 @@
+package chapter3;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+// WebServletAnnotation(アノーテーション)
+// URLの指定
+// ※クラス定義の直前に記述！
+@WebServlet( urlPatterns = {"/chapter3/hello2"} )
+
+// extends               = 定義
+public class Hello2 extends HttpServlet {
+    public void doGet (
+        // リクエストを渡す ↓
+        HttpServletRequest  request ,
+        // レスポンス生成して渡す ↑
+        HttpServletResponse response
+
+        // ServletException  : サーブレットでの問題
+        // IOException       : javaでの問題
+    ) throws ServletException , IOException {
+        PrintWriter out  = response.getWriter();
+
+        out.println("Hello こんにちは!");
+        out.println(new java.util.Date());
+    }
+}

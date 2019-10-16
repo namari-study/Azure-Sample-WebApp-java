671efab20739deca9b4d8f6e29dfc6a23094b01c
なまりゆか <namariko29@gmail.com>
2019-10-16 11:49:21 +0900

    エクリプステスト

--- webapps/book/WEB-INF/src/.bash_profile
+++
@@ -1,1 +0,0 @@
-export CLASSPATH=.:/usr/local/Cellar/tomcat/9.0.27/libexec/lib/servlet-api.jar
--- webapps/book/WEB-INF/src/chapter3/Hello.java
+++ webapps/book/WEB-INF/src/chapter3/Hello.java
@@ -1,31 +1,33 @@
 package chapter3;
 
-import java.io.*;
+import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.annotation.WebServlet;
-
-// WebServletAnnotation(アノーテーション)
-// URLの指定
-// ※クラス定義の直前に記述！
 @WebServlet( urlPatterns = {"/chapter3/hello"} )
 
-// extends               = 定義
+/**
+ * Servlet implementation class Hello
+ */
 public class Hello extends HttpServlet {
-    public void doGet (
-        // リクエストを渡す ↓
-        HttpServletRequest  request ,
-        // レスポンス生成して渡す ↑
-        HttpServletResponse response
-
-        // ServletException  : サーブレットでの問題
-        // IOException       : javaでの問題
-    ) throws ServletException , IOException {
-        PrintWriter out  = response.getWriter();
-
-        out.println("Hello こんにちは!");
-        out.println(new java.util.Date());
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
     }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
 }
